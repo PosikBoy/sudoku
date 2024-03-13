e55038541da1542b55fc7502c9b0ec9096640acf
PosikBoy <z.kalinichenko4@gmail.com>
2024-03-13 22:54:45 +0300

    anmksdc

--- src/services/auth/auth.helper.ts
+++ src/services/auth/auth.helper.ts
@@ -1,39 +1,39 @@
-import { IAuthResponse, ITokens } from "@/types/auth.interface";
-import Cookies from "js-cookie";
+// import { IAuthResponse, ITokens } from "@/types/auth.interface";
+// import Cookies from "js-cookie";
 
-export const saveTokensStorage = (data: ITokens) => {
-  Cookies.set("accessToken", data.accessToken);
-  Cookies.set("refreshToken", data.refreshToken);
-};
+// export const saveTokensStorage = (data: ITokens) => {
+//   Cookies.set("accessToken", data.accessToken);
+//   Cookies.set("refreshToken", data.refreshToken);
+// };
 
-export const saveToStorage = (data: IAuthResponse) => {
-  saveTokensStorage(data);
-  localStorage.setItem("user", JSON.stringify(data.user));
-};
+// export const saveToStorage = (data: IAuthResponse) => {
+//   saveTokensStorage(data);
+//   localStorage.setItem("user", JSON.stringify(data.user));
+// };
 
-export const removeInfoStorage = () => {
-  Cookies.remove("accessToken");
-  Cookies.remove("refreshToken");
-  localStorage.removeItem("user");
-};
+// export const removeInfoStorage = () => {
+//   Cookies.remove("accessToken");
+//   Cookies.remove("refreshToken");
+//   localStorage.removeItem("user");
+// };
 
-export const getAccessTokenStorage = () => {
-  const accessToken = Cookies.get("accessToken");
-  return accessToken || null;
-};
+// export const getAccessTokenStorage = () => {
+//   const accessToken = Cookies.get("accessToken");
+//   return accessToken || null;
+// };
 
-export const getRefreshTokenStorage = () => {
-  const refreshToken = Cookies.get("refreshToken");
-  return refreshToken || null;
-};
+// export const getRefreshTokenStorage = () => {
+//   const refreshToken = Cookies.get("refreshToken");
+//   return refreshToken || null;
+// };
 
-export const getUserStorage = () => {
-  const user = JSON.parse(localStorage.getItem("user") || "{}");
-  return user || null;
-};
+// export const getUserStorage = () => {
+//   const user = JSON.parse(localStorage.getItem("user") || "{}");
+//   return user || null;
+// };
 
-export const getUserTelegram = () => {
-  const username = window?.Telegram?.WebApp;
+// export const getUserTelegram = () => {
+//   const username = window?.Telegram?.WebApp;
 
-  return username || null;
-};
+//   return username || null;
+// };
--- src/services/auth/auth.service.ts
+++ src/services/auth/auth.service.ts
@@ -1,32 +1,31 @@
-import { IAuthResponse } from "@/types/auth.interface";
-import {
-  getRefreshTokenStorage,
-  saveToStorage,
-} from "@/services/auth/auth.helper";
+// import { IAuthResponse } from "@/types/auth.interface";
+// import {
+//   getRefreshTokenStorage,
+//   saveToStorage,
+// } from "@/services/auth/auth.helper";
 
-import { Cookies } from "js-cookie";
-import axios from "axios";
+// import axios from "axios";
 
-export const authService = {
-  //     async auth(type: "login" | "register", data) {
-  //         const response = awa
-  //   }
+// export const authService = {
+//   //     async auth(type: "login" | "register", data) {
+//   //         const response = awa
+//   //   }
 
-  async getNewTokens() {
-    const refreshToken = getRefreshTokenStorage();
-    const response = await axios.post<
-      string,
-      {
-        data: IAuthResponse;
-      }
-    >(
-      process.env.SERVER_URL + "/login/access-token",
-      { refreshToken },
-      { headers: { "Content-Type": "application/json" } }
-    );
-    if (response.data.accessToken) {
-      saveToStorage(response.data);
-    }
-    return response;
-  },
-};
+//   async getNewTokens() {
+//     const refreshToken = getRefreshTokenStorage();
+//     const response = await axios.post<
+//       string,
+//       {
+//         data: IAuthResponse;
+//       }
+//     >(
+//       process.env.SERVER_URL + "/login/access-token",
+//       { refreshToken },
+//       { headers: { "Content-Type": "application/json" } }
+//     );
+//     if (response.data.accessToken) {
+//       saveToStorage(response.data);
+//     }
+//     return response;
+//   },
+// };
